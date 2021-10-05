eebc44d65b75a50c558f400016ce98802ff3050d
phongtruong2106 <thpgamer42@gmail.com>
2021-10-05 22:51:52 +0700

    cập nhật

--- src/com/phongth2004110017/tuan04/lap4bai1.java
+++ src/com/phongth2004110017/tuan04/lap4bai1.java
@@ -1,5 +1,10 @@
 package com.phongth2004110017.tuan04;
 
 public class lap4bai1 {
-    
+    public static void main(String[] args) {
+        //tạo mới một đối tượng
+        sothuc st = new sothuc(); //tạo mới 1 đối tượng cho lớp sothuc
+        st.nhap();//gọi phương thức nhập của đối tượng st
+        st.xuat();//gọi phương thức xuất của đối tượng
+    }
 }
---
+++ src/com/phongth2004110017/tuan04/lap4bai2.java
@@ -0,0 +1,7 @@
+package com.phongth2004110017.tuan04;
+
+public class lap4bai2 {
+    public static void main(String[] args) {
+        
+    }
+}
