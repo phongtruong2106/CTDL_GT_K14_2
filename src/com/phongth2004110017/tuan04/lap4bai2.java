22afd5f656debdc4fd758605227c896fd87450e4
phongtruong2106 <thpgamer42@gmail.com>
2021-10-08 05:33:59 +0700

    Cập nhật

--- src/com/phongth2004110017/tuan04/lap4bai2.java
+++ src/com/phongth2004110017/tuan04/lap4bai2.java
@@ -2,6 +2,7 @@ package com.phongth2004110017.tuan04;
 
 public class lap4bai2 {
     public static void main(String[] args) {
-        
+        Danhsachhoten ds = new Danhsachhoten();
+        ds.hienthimenu();
     }
 }
