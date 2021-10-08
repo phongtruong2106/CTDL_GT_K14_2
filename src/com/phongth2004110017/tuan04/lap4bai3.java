8014482ed97abfc9d16660bfada0460878e00545
phongtruong2106 <thpgamer42@gmail.com>
2021-10-08 12:13:51 +0700

    Cập nhật

--- src/com/phongth2004110017/tuan04/lap4bai3.java
+++ src/com/phongth2004110017/tuan04/lap4bai3.java
@@ -1,5 +1,8 @@
 package com.phongth2004110017.tuan04;
 
 public class lap4bai3 {
-    
+    public static void main(String[] args) {
+        danhsachSanPham ds = new danhsachSanPham();
+        ds.menu();
+    }
 }
