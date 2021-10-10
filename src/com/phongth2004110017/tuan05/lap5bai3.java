f8581dac0977661563ffc9159fffc54dae6b4a80
phongtruong2106 <thpgamer42@gmail.com>
2021-10-10 11:58:17 +0700

    cập nhật

---
+++ src/com/phongth2004110017/tuan05/lap5bai3.java
@@ -0,0 +1,8 @@
+package com.phongth2004110017.tuan05;
+
+public class lap5bai3 {
+    public static void main(String[] args) {
+        danhsachSanPham ds = new danhsachSanPham();
+        ds.menu();
+    }
+}
