2178bc83e5c410029db29df8e3485be2e98b6998
phongtruong2106 <thpgamer42@gmail.com>
2021-10-13 08:28:04 +0700

    cập nhật

--- src/com/phongth2004110017/tuan04/lap4bai1.java
+++ src/com/phongth2004110017/tuan04/lap4bai1.java
@@ -1,10 +1,38 @@
 package com.phongth2004110017.tuan04;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class lap4bai1 {
     public static void main(String[] args) {
-        //tạo mới một đối tượng
-        sothuc st = new sothuc(); //tạo mới 1 đối tượng cho lớp sothuc
-        st.nhap();//gọi phương thức nhập của đối tượng st
-        st.xuat();//gọi phương thức xuất của đối tượng
-    }
+        ArrayList<Double> list = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        String qd; //quy định nhập
+       boolean yn = true;
+       while(yn){
+           System.out.println("nhập số: ");
+           Double x = Double.parseDouble(sc.nextLine());//nhập số từ bàn phím
+           //đưa số vừa nhập vào list
+           list.add(x);
+           //điều khiển vòng lặp
+           qd = sc.nextLine();
+           switch(qd){
+               case "y":
+                   yn = true;
+                   break;
+               case "n":
+                   yn = false;
+                   break;
+               default:
+                   System.err.println("hãy nhập lại số");
+           }
+           double tong = 0;
+           for (Double n : list){
+               System.out.println(n);
+               tong += n;
+           }
+           System.out.println("tổng là : " + tong);
+           sc.close();
+        }
+}
 }
