f9437df618de09cc0da129590634841ab0717e14
phongtruong2106 <thpgamer42@gmail.com>
2021-10-17 19:19:07 +0700

    cập nhật

--- src/com/phongth2004110017/tuan03/lap3bai2.java
+++ src/com/phongth2004110017/tuan03/lap3bai2.java
@@ -2,11 +2,12 @@ package com.phongth2004110017.tuan03;
 
 public class lap3bai2 {
     public static void main(String[] args) {
-        for(int i = 2; i<=9; i++ ){
-            for(int j = 1; j<=9; j++){
-                System.out.print(i + " x " + j + " = " + i * j + "\t");
-            }
-            System.out.println(" ");
-        }
+       for(int i = 1; i<=9; i++){
+           for(int j =1; j<=10; j++){
+               System.out.printf(" |%d x %d = %d|", i, j, i * j);
+           }
+           System.out.println();
+       }
+       
     }
 }
