357ec1c6749ba69faf16c673777a3cc2f6f7dc61
phongtruong2106 <thpgamer42@gmail.com>
2021-11-06 17:55:31 +0700

    cập nhật

--- src/com/phongth2004110017/tuan08/BinaryTree.java
+++ src/com/phongth2004110017/tuan08/BinaryTree.java
@@ -5,11 +5,12 @@ import java.util.Queue;
 
 public class BinaryTree {
     Node root;
+
     private Node addRecursive(Node current, int value) {
         if (current == null) {
             return new Node(value);
         }
-    
+
         if (value < current.value) {
             current.left = addRecursive(current.left, value);
         } else if (value > current.value) {
@@ -18,9 +19,10 @@ public class BinaryTree {
             // value already exists
             return current;
         }
-    
+
         return current;
     }
+
     public void add(int value) {
         root = addRecursive(root, value);
     }
@@ -39,39 +41,70 @@ public class BinaryTree {
 
         return bt;
     }
-    public static void breadthFirst(Node node){
-        if(node == null){
+
+    public static void breadthFirst(Node node) {
+        if (node == null) {
             System.out.println("Empty Tree");
             return;
         }
 
         Queue<Node> queue = new LinkedList<>();
         queue.add(node);
-        while(!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             Node current = queue.poll();
             System.out.println(current.value);
-            if(current.left !=null) {
+            if (current.left != null) {
                 queue.add(current.left);
             }
 
-            if(current.right !=null) {
+            if (current.right != null) {
                 queue.add(current.right);
             }
         }
     }
-    //tìm 1 phần tử
+
+    // tìm 1 phần tử
     private boolean containsNodeRecursive(Node current, int value) {
         if (current == null) {
             return false;
-        } 
+        }
         if (value == current.value) {
             return true;
-        } 
-        return value < current.value
-          ? containsNodeRecursive(current.left, value)
-          : containsNodeRecursive(current.right, value);
+        }
+        return value < current.value ? containsNodeRecursive(current.left, value)
+                : containsNodeRecursive(current.right, value);
     }
+
     public boolean containsNode(int value) {
         return containsNodeRecursive(root, value);
     }
+
+    //
+    public static void traverseInOrder(Node node) {
+        if (node != null) {
+            traverseInOrder(node.left);
+            System.out.print(" " + node.value);
+            traverseInOrder(node.right);
+        }
+    }
+
+    //
+    public static void traversePreOrder(Node node) {
+        if (node != null) {
+            System.out.print(" " + node.value);
+            traversePreOrder(node.left);
+            traversePreOrder(node.right);
+        }
+    }
+
+    //
+    public static void traversePostOrder(Node node) {
+        if (node != null) {
+            traversePostOrder(node.left);
+            traversePostOrder(node.right);
+            System.out.print(" " + node.value);
+        }
+    }
+
+   
 }
--- src/com/phongth2004110017/tuan08/BinaryTreeTestDrive.java
+++ src/com/phongth2004110017/tuan08/BinaryTreeTestDrive.java
@@ -1,12 +1,20 @@
 package com.phongth2004110017.tuan08;
 
 public class BinaryTreeTestDrive {
-      public static void main(String[] args) {
-          BinaryTree binaryTree = BinaryTree.createBinaryTree();
-          
-           BinaryTree.breadthFirst(binaryTree.root);
+    public static void main(String[] args) {
+        BinaryTree binaryTree = BinaryTree.createBinaryTree();
+        BinaryTree.breadthFirst(binaryTree.root);
 
-           
-      }
+        System.out.println("duyệt theo thứ tự bao gồm việc truy cập đầu tiên vào cây con bên trái, sau đó đến nút gốc và cuối cùng là cây con bên phải");
+        BinaryTree.traverseInOrder(binaryTree.root);
+
+        System.out.println(" \n Đặt trước các lượt truy cập duyệt qua nút gốc, sau đó đến cây con bên trái và cuối cùng là cây con bên phả");
+        BinaryTree.traversePreOrder(binaryTree.root);
+
+        System.out.println(" \n Duyệt theo thứ tự sau truy cập vào cây con bên trái, cây con bên phải và nút gốc ở cuối");
+        BinaryTree.traversePostOrder(binaryTree.root);
+
+   
+    }
 
 }
